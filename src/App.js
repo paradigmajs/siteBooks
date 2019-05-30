7099ed6f3ebec44683b09bc2050827f28bd34a3e
Sultan Eraliev <sultan.eraliev@gmail.com>
2019-05-30 10:54:30 +0600

    createdHeader&Nav

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Somepage from './components/somepage'
-
+import Banner from './components/Banner';
+import bannerSecond from './components/bannerSecond';
 import { connect } from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 // рекомендовано
@@ -15,31 +16,21 @@ class App extends React.Component {
   render(){
     return(
       <BrowserRouter history={history}>
-        <div>
-          <ul>
-            <li>
-              <Link to="/"></Link>
-            </li>
-            <li>
-              <Link to="/somepage">somepage</Link>
-            </li>
-            <li>
-              <Link to="/"></Link>
-            </li>
-          </ul>  
+      	<div id="page-wrapper">
+          <header id="header">
+          <h1 id="logo">
+              <Link to="/">Theos and Sophia</Link>
+          </h1>
+          </header>
+          <Banner/>
+          <bannerSecond/>
         </div>
-
         <hr/>
 
         <Route exact path = "/" component={Home} />
-        <Route path = "/somepage" component={Somepage} />
-
-      </BrowserRouter>
+        </BrowserRouter>
     )
   }
 }
-
-
-
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+class Header extends React.Component{
+    render () {
+     
+      
+  }
+};
+
+export default Header;
